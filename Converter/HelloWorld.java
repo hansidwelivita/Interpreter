f6299d6bc463abfe5f15f49f977aa8e4f194ba67
JonKue <jonathan.kue@gmail.com>
2015-06-28 01:02:17 -0500

    Add Hello World Class

---
+++ Converter/HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    /**
+     * Constructor for objects of class HelloWorld
+     */
+    public HelloWorld()
+    {
+        System.out.println("Hello World!");
+    }
+}
