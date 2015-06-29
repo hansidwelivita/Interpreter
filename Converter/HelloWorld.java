b209f84952303f8a363d5b2267d868e268c38ef7
JonKue <jonathan.kue@gmail.com>
2015-06-29 09:23:32 -0500

    add function and comments to test

--- Converter/HelloWorld.java
+++ Converter/HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld {
     HelloWorld() {
         System.out.println("Hello World!");
+        //test comment
+    }
+    public void testFunction() {
+        System.out.println("Testing");
+        //function test
     }
 }
