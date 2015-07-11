9db7b2dbf3c3bc26a13012b521c667d7a3370386
sirtuck32 <t.long32@hotmail.com>
2015-07-10 22:00:01 -0500

    Updated to main?
    Missing proper HelloWorld?

--- Converter/python/HelloWorld.py
+++ Converter/python/HelloWorld.py
@@ -1,17 +1,23 @@
 class HelloWorld():
-	def __init__(self):
-		self.z = 3
+	HelloWorld(self, i=None, l=None, d=None, test=None):
 		self.i = 0
 		self.x = 0
-		print("Hello World!")
-		#test comment
-	def __init__(self):
 		self.z = 3
-		self.i = 0
-		self.x = 0
-		print("Constructor with inp)
-	def testFunction(test,testString ):
-		print("Testing")
+
+		if(i is None and l is None and d is None and test is None):
+			print 'Hello World!' 
+			#test comment
+		if((i is not None and isinstance(i, int) and l is None and d is None and test is None):
+			print 'Constructor with input.' 
+		if(i is None and (l is not None and isinstance(l, int) and (d is not None and isinstance(d, double) and test is None):
+			print 'Second constructor with same input type' 
+		if(i is None and l is None and (d is not None and isinstance(d, double) and test is not None):
+			print 'Constructor with two inputs and variables.' 
+			instance_variable = 0
+		if(i is None and l is None and (d is not None and isinstance(d, float) and test is not None):
+			print 'Constructor with same name input different type.' 
+			instance_variable = 0
+	def testFunction(test,testString):
+		print 'Testing' 
 		#function test
-	def testReturnFunction():
-		return testString
+	def testIndexFunction():
