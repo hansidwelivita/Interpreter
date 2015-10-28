c10ad0a98ebec14da2b7d680ea750aa641d34b75
sirtuck32 <t.long32@hotmail.com>
2015-10-28 16:07:56 -0500

    ..

--- Converter/HelloWorld.java
+++ Converter/HelloWorld.java
@@ -11,26 +11,14 @@ class HelloWorld {
         System.out.println("Constructor with input.");
     }
     
-    HelloWorld(int l, double d) {
-        System.out.println("Second constructor with same input type");
-    }
-    
-    HelloWorld(String test, double d) {
-        System.out.println("Constructor with two inputs and variables.");
-        int instance_variable = 0;
-    }
-    
-    HelloWorld(int test, float d) {
-        System.out.println("Constructor with same name input different type.");
-        int instance_variable = 0;
-    }
-    
     public void testFunction(int test, String testString) {
         System.out.println("Testing");
         //function test
     }
     
-    public void testIndexFunction(){
+    public String testReturnFunction(){
+        String returnString = "";
+        return returnString;
     }
     
     int z = 3;
